9fef33738f9871a5ac6fe57ce4154120aba6aca3
itsme-swe <me.harshmehra07@gmail.com>
2024-08-31 08:01:15 +0530

    update

---
+++ Chapter07_Objects/4_objectsPartFour.js
@@ -0,0 +1,14 @@
+/*
+1️⃣ Destructuring Objects : Ham destructuring isliye karte hai jissae hame baar baar " obj_Name.key " na use karna pade kisi keys ki value ko access karne kae liye.
+*/
+const course = {
+  courseName: "NodeJS",
+  price: 2999,
+  master: "Saini ji",
+  topics: ["Express", "Database", "Middleware", "API Building"],
+};
+
+const { courseName, price } = course; // Syntax: const {keys} = obj_Name; Hum curly braces'{}' kae andar vo object ki keys pass karte hai joh hame destructure karni hai
+
+console.log(courseName); // output: NodeJS
+console.log(price); // output: 2999

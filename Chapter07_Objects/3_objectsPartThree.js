1359000ecfc73ded435d1fa19e26389cff3e6407
itsme-swe <me.harshmehra07@gmail.com>
2024-08-31 07:41:44 +0530

    update

--- Chapter07_Objects/3_objectsPartThree.js
+++ Chapter07_Objects/3_objectsPartThree.js
@@ -14,3 +14,25 @@ newUser.lstLoggedIn = ["Monday", "Wednesday"];
 console.log(newUser);
 
 const tinderUser = {}; //🍁 Non-Singleton Object
+
+console.log();
+
+const obj1 = {
+  a: 101,
+  b: 102,
+  c: 103,
+};
+
+const obj2 = {
+  1: "a",
+  2: "b",
+  3: "c",
+};
+
+const obj3 = { ...obj1, ...obj2 }; // By using spread operator we can merge two objects together
+
+const assignOperator = Object.assign(obj1, obj2); // We can also use ' Objects.assign(obj1, obj2) ' operator to merge the objects.
+
+console.log(obj3);
+
+console.log(assignOperator);

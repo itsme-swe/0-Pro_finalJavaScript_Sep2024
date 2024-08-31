b8b7b6928727920cc6ddbc2656ca0faeaf66825c
itsme-swe <me.harshmehra07@gmail.com>
2024-08-31 07:33:57 +0530

    update

---
+++ Chapter07_Objects/3_objectsPartThree.js
@@ -0,0 +1,16 @@
+/*
+1️⃣ Singleton and Non-Singleton method to declare Object
+
+*/
+
+//🍁 Singleton Method
+const newUser = new Object();
+
+newUser.name = "Max";
+newUser.age = 32;
+newUser.location = "Delhi";
+newUser.lstLoggedIn = ["Monday", "Wednesday"];
+
+console.log(newUser);
+
+const tinderUser = {}; //🍁 Non-Singleton Object

74b81e329aab35a80bee44d4af8eaaea34de4378
itsme-swe <me.harshmehra07@gmail.com>
2024-09-01 07:12:17 +0530

    update

--- Chapter08_Functions/2_functionsPartTwo.js
+++ Chapter08_Functions/2_functionsPartTwo.js
@@ -9,3 +9,21 @@ function loginUserMessage(username) {
 console.log(loginUserMessage("Harsh")); // output: Harsh just logged in !
 
 console.log(loginUserMessage()); // Agar hum koi bhi argument pass nhi karenge toh undefined return hoga.
+
+console.log();
+
+//🔸 Aub is function mae hum default value set karenge agar user kuch bhi pass nhi karega tub bhi hamari default value print hogi.
+function loginDetail(user = "Atom") {
+  if (!user) {
+    console.log("Enter valid username");
+    return;
+  }
+  return `${user} logged in !!`;
+}
+
+const user1 = loginDetail("Sam");
+
+const user2 = loginDetail();
+
+console.log(user1); // output: Sam logged in !!
+console.log(user2); // output: Atom logged in !!

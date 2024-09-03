775f1c24741458c29ea7f37222b7e54966d3a735
itsme-swe <me.harshmehra07@gmail.com>
2024-09-03 20:24:21 +0530

    update

---
+++ Chapter11_ControlFlowInJS/1_ifElseStatement.js
@@ -0,0 +1,19 @@
+/*
+1️⃣ if else Statement : Agar hamari condition true hoti hai tub he hum ' if(condition){ } ' kae scope kae andar jaate hai. ' if ' condition mae hum comparison operators use karte hai. E.g: < , > , <= , >= , !=(not equal) ( 3 != 2 ) » iska matalab hai kya three, two kae equal hai ,  ===  
+
+2️⃣ 
+*/
+
+if (2 == "2") {
+  console.log("True");
+}
+
+// ans: iska output 'true' ayega kyu '==' operator sirf value check karta hai
+
+console.log();
+
+if (2 === "2") {
+  console.log("False");
+}
+
+// ans: yae code execute nhi hoga kyuki '===' operator value kae sath data type bhi check karta hai

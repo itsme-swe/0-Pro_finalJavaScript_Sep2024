2894b221c1c149bf5b9c9983b04be9ca29089db2
itsme-swe <me.harshmehra07@gmail.com>
2024-09-04 19:43:11 +0530

    update

--- Chapter11_ControlFlowInJS/1_ifElseStatement.js
+++ Chapter11_ControlFlowInJS/1_ifElseStatement.js
@@ -1,7 +1,11 @@
 /*
 1️⃣ if else Statement : Agar hamari condition true hoti hai tub he hum ' if(condition){ } ' kae scope kae andar jaate hai. ' if ' condition mae hum comparison operators use karte hai. E.g: < , > , <= , >= , !=(not equal) ( 3 != 2 ) » iska matalab hai kya three, two kae equal hai ,  ===  
 
-2️⃣ 
+2️⃣ ' && ' logical AND operator, ismae dono condition true honi chaiye tubhe scope kae andar vala code execute hota hai.
+
+3️⃣ ' || ' logical OR opeartor, ismae koi si bhi 1 condition true honi chaiye toh code execute ho jaata hai.  
+
+
 */
 
 if (2 == "2") {

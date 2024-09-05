af71675e321861ea0ebbe3f5716544b65720890f
itsme-swe <me.harshmehra07@gmail.com>
2024-09-05 07:08:34 +0530

    update

--- Chapter11_ControlFlowInJS/1_ifElseStatement.js
+++ Chapter11_ControlFlowInJS/1_ifElseStatement.js
@@ -5,6 +5,8 @@
 
 3️⃣ ' || ' logical OR opeartor, ismae koi si bhi 1 condition true honi chaiye toh code execute ho jaata hai.  
 
+4️⃣ Nullish Coalescing Operator (??): null undefined »  yae hum tub use karte hai jub hum database sae cheeje call kar rahe hote hai. Agar value null ayi toh hamara code fate na vo usae null assign kardega aur agar value ayi hai toh value assign kardega.
+
 
 */
 
@@ -21,3 +23,15 @@ if (2 === "2") {
 }
 
 // ans: yae code execute nhi hoga kyuki '===' operator value kae sath data type bhi check karta hai
+
+console.log();
+
+let value1;
+
+value1 = null ?? 10;
+
+console.log(value1); // yaha pae output 10 ayega kyuki value present hai
+
+const val1 = null ?? 50 ?? 10;
+
+console.log(val1); // aur yaha pae '50' output ayega kyuki jub bhi hum null coalesing use karte hai joh bhi value phelae aati hai vo assign ho jati hai.
---
+++ Chapter11_ControlFlowInJS/4_terniaryOperator.js
@@ -0,0 +1,14 @@
+/*
+💥 Terniary opearator bhi if else condition ki tarah he use hoti hai.
+  Syntax : condition ? true : false
+*/
+
+const curvPrice = 2200000;
+
+curvPrice >= 2100000
+  ? console.log("Diesel Model")
+  : console.log("Petrol Model");
+
+/*
+Yaha hum check kar rahe hai agar hamari gadi ki price 21 lac sae upper hai toh 'Diesel Model' print karo varna 'Petrol Model'
+*/

b10f548cda2c0c10a20e8786a6302af6e60a2c64
itsme-swe <me.harshmehra07@gmail.com>
2024-09-05 20:57:00 +0530

    update

---
+++ Chapter12_Looping/6_forEachLoop.js
@@ -0,0 +1,19 @@
+/*
+1️⃣ ' forEach ' loop »» Most commonly used loop in JS. 'forEach' loop 1 higher order function considered hota hai. forEach( function(eleOfArray) {} ) apane andar callback as parameter leta hai. Aub yaha hum 'arrow function' bhi use kar sukte hai.
+*/
+
+const coding = ["JS", "Java", "Python", "Cpp", "Ruby"];
+
+coding.forEach((ele) => {
+  console.log(ele);
+});
+
+/*
+output:   JS
+          Java
+          Python
+          Cpp
+          Ruby
+*/
+
+console.log();
